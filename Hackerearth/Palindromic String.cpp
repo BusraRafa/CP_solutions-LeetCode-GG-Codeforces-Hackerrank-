1672cec5bcadc8d94885ad264baa7a01358bf7fb
Busra Kamal Rafa <brafa263.3@gmail.com>
2024-12-07 21:27:11 +0600

    Time 0.03969s	Memory 2kb

---
+++ Hackerearth/Palindromic String.cpp
@@ -0,0 +1,21 @@
+#include<bits/stdc++.h>
+using namespace std;
+int main(){
+string s;
+cin>>s;
+bool f=true;
+
+int n=(s.length()/2);
+
+for (int i=0;i<n;i++)
+  {
+	if(s[i]!=s[s.length()-i-1])
+		{f=false;
+		break;}
+	}
+   if(f==true){
+	cout<<"YES";
+   }else
+   cout<<"NO";
+   }
+
