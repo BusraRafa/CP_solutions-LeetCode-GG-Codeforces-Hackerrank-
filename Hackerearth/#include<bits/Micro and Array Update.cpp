4c799f9eff640197397d4b899a0d00690df71e50
Busra Kamal Rafa <brafa263.3@gmail.com>
2024-12-12 02:04:47 +0600

    Time (sec)  1.53722  Memory (KiB)  956

---
+++ Hackerearth/#include<bits/Micro and Array Update.cpp
@@ -0,0 +1,22 @@
+#include<bits/stdc++.h>
+using namespace std;
+int main() {
+
+    int t;   
+	cin >> t;
+
+    while(t--) {
+ 		int n, k,diff = 0,temp = 0;; 
+        cin >> n >> k;     
+		vector<int>arr(n);
+        for(int i = 0; i < n; i++) {
+		 cin >> arr[i];        
+		 }
+        for(int i = 0; i < n; i++) {
+			  diff = k - arr[i]; 
+			  if(diff > temp) {        
+				 temp = diff;  }
+		       }
+        cout << temp << endl;    
+			}
+		}
