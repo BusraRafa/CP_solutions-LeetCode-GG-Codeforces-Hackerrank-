87c773e2a88ded795ce9a4f5add0292a9bc398ea
Busra Kamal Rafa <brafa263.3@gmail.com>
2024-12-20 04:31:06 +0600

    Time (ms) 0 Memory (MB)12.4

--- LeetCode/88. Merge Sorted Array/Merge Sorted Array.cpp
+++ LeetCode/88. Merge Sorted Array/Merge Sorted Array.cpp
@@ -1,17 +1,15 @@
 class Solution {
 public:
     void merge(vector<int>& nums1, int m, vector<int>& nums2, int n) {
-       vector<int>n1;
-       for(int i=0;i<m;i++){
-            n1.push_back(nums1[i]);
+      int n2=nums2.size();
+      if(n2!=0){
+       for(int i=0;i<n2;i++){
+        nums1[m+i]=nums2[i];
        }
-       int n2=nums2.size();
-       if(n2!=0){
-        for(int i=0;i<n2;i++){
-          n1.push_back(nums2[i]);  
-        }
     }
-        sort(n1.begin(),n1.end());
+        sort(nums1.begin(),nums1.end());
+       
+        
     }
 };
 
