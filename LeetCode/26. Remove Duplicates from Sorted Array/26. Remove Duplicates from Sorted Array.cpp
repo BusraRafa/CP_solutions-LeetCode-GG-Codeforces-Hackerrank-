aa500a6ea081f413a65ef00058cd4ff302b8a15f
Busra Kamal Rafa <brafa263.3@gmail.com>
2024-12-21 03:58:44 +0600

    Time (ms) 0 Memory (MB)23.43

--- LeetCode/26. Remove Duplicates from Sorted Array/26. Remove Duplicates from Sorted Array.cpp
+++ LeetCode/26. Remove Duplicates from Sorted Array/26. Remove Duplicates from Sorted Array.cpp
@@ -20,12 +20,13 @@ public:
 class Solution {
 public:
     int removeDuplicates(vector<int>& nums) {
-        int j = 1;
+        int j = 0;
         for(int i = 1; i < nums.size(); i++){
-            if(nums[i] != nums[i - 1]){
-                nums[j] = nums[i];
+            if(nums[i] == nums[i - 1]){
                 j++;
             }
+	    else
+     		nums[i-j]=nums[i];
         }
         return j;
     }
