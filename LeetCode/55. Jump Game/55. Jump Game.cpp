b7de28cf7b1dc281159f5d719d2ec0a8615e1110
Busra Kamal Rafa <brafa263.3@gmail.com>
2024-12-23 03:07:04 +0600

    Time (ms) 0 Memory (MB)52.14

--- LeetCode/55. Jump Game/55. Jump Game.cpp
+++ LeetCode/55. Jump Game/55. Jump Game.cpp
@@ -1,6 +1,19 @@
 class Solution {
 public:
     bool canJump(vector<int>& nums) {
+        int m=0;
+	for(int i=0;i<nums.size();i++){
+		if(m<i){
+			return false;
+		}
+        m=max(m,nums[i]+i);
+		
+    }
+
+
+/*class Solution {
+public:
+    bool canJump(vector<int>& nums) {
         int cur=nums[0];
         for(int i=1;i<nums.size();i++){
           if(cur!=0){
@@ -12,4 +25,4 @@ public:
         } 
         return true;
     }
-};
+};*/
