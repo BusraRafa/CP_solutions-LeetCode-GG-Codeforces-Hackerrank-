4a01814c1ca0cd26f6ffac41b60f70427ad41d14
Busra Kamal Rafa <brafa263.3@gmail.com>
2024-12-29 22:07:43 +0600

    Time (ms) 43 Memory (MB)42.15

---
+++ LeetCode/238. Product of Array Except Self/238. Product of Array Except Self.cpp
@@ -0,0 +1,21 @@
+class Solution {
+public:
+    vector<int> productExceptSelf(vector<int>& nums) {
+        vector<int> prefix(nums.size());
+        vector<int> sufix(nums.size());
+        vector<int> v(nums.size());
+        //1 2 3 4 
+        prefix[0]=1;
+        for(int i=1;i<nums.size();i++){
+        prefix[i]= prefix[i-1] *nums[i-1];
+        }
+        sufix[nums.size()-1]=1;
+        for(int i=nums.size()-2;i>=0;i--){
+        sufix[i]=sufix[i+1]*nums[i+1];
+        }
+        for(int i=0;i<nums.size();i++){
+            v[i]=prefix[i]*sufix[i];
+        }
+        return v;
+    }
+};
