c910676d03da2911e97d5eb30eff34fe9c25a1f1
Busra Kamal Rafa <brafa263.3@gmail.com>
2025-01-02 23:18:23 +0600

    Time (ms)5 Memory (MB)13.66

--- LeetCode/13. Roman to Integer/13. Roman to Integer.cpp
+++ LeetCode/13. Roman to Integer/13. Roman to Integer.cpp
@@ -5,7 +5,7 @@ public:
        {'V',5},{'X',10},{'L',50},{'C',100},{'D',500},{'M',1000}};
        int sum=0;
        for(int i=0;i<s.size();i++){
-        if(mp[s[i]]<mp[s[i+1]]){
+        if(mp[s[i]]<mp[s[i+1]]){//mp['I']=1
             sum=sum-mp[s[i]];
         }else{
             sum=sum+mp[s[i]];
