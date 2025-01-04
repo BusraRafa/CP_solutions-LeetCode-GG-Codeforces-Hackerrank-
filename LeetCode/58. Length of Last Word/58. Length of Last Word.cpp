28f1516d6aeb275c41e75f10874cc978ddbcf5ec
Busra Kamal Rafa <brafa263.3@gmail.com>
2025-01-04 21:17:05 +0600

    Time (ms)0 Memory (MB)8.87

--- LeetCode/58. Length of Last Word/58. Length of Last Word.cpp
+++ LeetCode/58. Length of Last Word/58. Length of Last Word.cpp
@@ -1,13 +1,13 @@
 class Solution {
 public:
     int lengthOfLastWord(string s) {
-        int count=0,sp=0;
+        int count=0;
         for(int i=s.size()-1;i>=0;i--){
-            if(s[i]!=' '){
-            count=count+1;
+            if(s[i]==' ' && count!=0){
+            break;
             }
-            else if(s[i]==' ' && count!=0 ){
-                break;
+            else if(s[i]!=' '){
+                count+=1;
             }
         }
         return count;
