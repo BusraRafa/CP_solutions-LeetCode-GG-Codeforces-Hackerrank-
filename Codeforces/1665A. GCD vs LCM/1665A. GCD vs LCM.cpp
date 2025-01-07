96e5dd415980404e3fa75acecd3d40ded24fbe2a
Busra Kamal Rafa <brafa263.3@gmail.com>
2025-01-08 00:53:49 +0600

    Time (ms)77 Memory (KB)0

---
+++ Codeforces/1665A. GCD vs LCM/1665A. GCD vs LCM.cpp
@@ -0,0 +1,27 @@
+#include <bits/stdc++.h>
+using namespace std;
+int main() {
+	int t;
+    cin>>t;
+    while(t--){
+        int n;
+        cin>>n;
+        if(n<4){
+            return 0;
+        }
+        int a=n-3;
+        cout<<a<<" "<<1<<" "<<1<<" "<<1;
+        cout<<endl;
+    }
+   
+}
+
+
+	
+
+	
+	
+
+
+
+
