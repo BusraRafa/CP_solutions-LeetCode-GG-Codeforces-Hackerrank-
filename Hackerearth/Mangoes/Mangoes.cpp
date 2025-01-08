8944db580debcd6c39d5a03d199b579210790488
Busra Kamal Rafa <brafa263.3@gmail.com>
2025-01-09 00:00:05 +0600

    Time (ms)0.06273 Memory (kb) 2

---
+++ Hackerearth/Mangoes/Mangoes.cpp
@@ -0,0 +1,13 @@
+#include <iostream>
+using namespace std;
+int main() {
+	int num;
+	cin >> num;    
+	//the product of evn num is evn
+	if((num%2)==0 && num!=2){
+		cout<<"YES"<<endl;
+	}else
+	cout<<"NO"<<endl;
+	
+	
+}
