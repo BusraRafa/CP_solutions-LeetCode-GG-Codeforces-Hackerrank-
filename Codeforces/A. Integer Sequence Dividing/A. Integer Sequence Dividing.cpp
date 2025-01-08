2d59f3405e1ae104538992a3669bbf5fdbf0cd84
Busra Kamal Rafa <brafa263.3@gmail.com>
2025-01-08 23:51:41 +0600

    Time (ms) 62 Memory (kb)52

---
+++ Codeforces/A. Integer Sequence Dividing/A. Integer Sequence Dividing.cpp
@@ -0,0 +1,12 @@
+#include <bits/stdc++.h>
+using namespace std;
+int main() {
+	int n;
+    cin>>n;
+    long long sum=(n*(n+1))/2;
+    if(sum%2==0){
+        cout<<0;
+    }else
+    cout<<1;
+ 
+}
