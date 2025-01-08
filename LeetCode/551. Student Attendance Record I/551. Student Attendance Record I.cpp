1f5e51e316af5f2553cf1e2ddecac980c2219088
Busra Kamal Rafa <brafa263.3@gmail.com>
2025-01-09 00:10:30 +0600

    Time (ms) 0 Memory (MB)8.42

---
+++ LeetCode/551. Student Attendance Record I/551. Student Attendance Record I.cpp
@@ -0,0 +1,23 @@
+class Solution {
+public:
+//LALL--f expectred t
+    bool checkRecord(string s) {
+        int A=0,L=0;
+       for(int i=0;i<s.length();i++){
+        if(s[i]=='A'){
+            A++;
+        }//LLL
+        else if((s[i]=='L') && (i+1<s.length() && s[i+1]=='L') && (i+2<s.length() & s[i+2]=='L')){
+            L=L+3;;
+            i=i+2;
+        }
+       }
+       if(A>1| L>=3){
+        return false;
+       }
+       else
+        return true;
+         
+    
+    }
+};
