ddc7a7e4f79074daa6f649c922c38d48e04fd2f8
Busra Kamal Rafa <brafa263.3@gmail.com>
2025-01-10 04:46:32 +0600

    Time (ms)14 Memory (MB)11.06

--- LeetCode/1358. Number of Substrings Containing All Three Characters/1358. Number of Substrings Containing All Three Characters.cpp
+++ LeetCode/1358. Number of Substrings Containing All Three Characters/1358. Number of Substrings Containing All Three Characters.cpp
@@ -8,9 +8,23 @@ public:
            if(lastchar[0]!=-1 && lastchar[1]!=-1 && lastchar[2]!=-1){
               count=count+(1+min({lastchar[0], lastchar[1],lastchar[2]}));
                // count=count+(1+min(min(lastchar[0], lastchar[1]),lastchar[2]));
-           }
-           
+           } 
         }
         return count;
     }
 };
+/*
+    int count=0;
+    vector<int> lastchar(3,-1);
+    for(int i=0;i<s.size();i++){
+       // lastchar[s[i]-'b']=i;
+        if(s[i]=='b'){lastchar[0]=i;}
+        else if(s[i]=='f'){lastchar[1]=i;}
+        else if(s[i]=='g'){lastchar[2]=i;}
+        if(lastchar[0]!=-1 && lastchar[1]!=-1 && lastchar[2]!=-1 ){
+        count=count+(1+min({lastchar[0],lastchar[1],lastchar[2]}));
+        }
+    }
+    cout<<count;
+}
+*/
